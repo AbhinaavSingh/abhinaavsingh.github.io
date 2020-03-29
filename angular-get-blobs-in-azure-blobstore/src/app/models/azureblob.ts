dd2d7421803d4c2ea7533998b00efded097c4e84
Abhinaav Singh <asingh75@slb.com>
2020-03-29 21:55:47 +0530

    main changes

--- angular-get-blobs-in-azure-blobstore/src/app/models/azureblob.ts
+++
@@ -1,9 +0,0 @@
-export class Azureblob {
-    blobName: string;
-    blobURL: string;
-
-  constructor(private blobName1: string, private url: string) {
-    this.blobName = blobName1;
-    this.blobURL = url;
-  }
-}
